ee5f9f682669db53f4dadfedde9c0183d29eacb9
genesis_Bunny <genesis26bunny@gmail.com>
2026-07-22 22:37:03 +0300

    Create agent_role.py

---
+++ core/agent_role.py
@@ -0,0 +1,24 @@
+from enum import Enum
+
+
+class AgentRole(Enum):
+
+    MANAGER = "manager"
+
+    PLANNER = "planner"
+
+    DEVELOPER = "developer"
+
+    REVIEWER = "reviewer"
+
+    OPTIMIZER = "optimizer"
+
+    RESEARCHER = "researcher"
+
+    WRITER = "writer"
+
+    DESIGNER = "designer"
+
+    MARKETER = "marketer"
+
+    PACKAGER = "packager"
