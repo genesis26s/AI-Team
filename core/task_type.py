606cd209c0727b2bc39f8e9bdcba6e9018b0ad1b
genesis_Bunny <genesis26bunny@gmail.com>
2026-07-22 22:33:31 +0300

    Create task_type.py

---
+++ core/task_type.py
@@ -0,0 +1,24 @@
+from enum import Enum
+
+
+class TaskType(Enum):
+
+    GREETING = "greeting"
+
+    CHAT = "chat"
+
+    CODING = "coding"
+
+    DEBUGGING = "debugging"
+
+    RESEARCH = "research"
+
+    WRITING = "writing"
+
+    MATH = "math"
+
+    VISION = "vision"
+
+    FILE = "file"
+
+    UNKNOWN = "unknown"
